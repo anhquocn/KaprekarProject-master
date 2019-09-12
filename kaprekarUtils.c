faa81c1840e13eaf0298ec64b79a4509be6cfc5d
anhquocn <54682359+anhquocn@users.noreply.github.com>
2019-09-12 18:51:35 -0500

    I changed number0fDigits to numDigits.

--- kaprekarUtils.c
+++ kaprekarUtils.c
@@ -38,7 +38,8 @@ int isKaprekar(int n) {
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<=numDigits; i++) {
+    // I changed number0fDigits to numDigits.
     //increase the modulus by a factor of 10
     modulous *= 10;
 
